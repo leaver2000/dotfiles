7a5c62a9e4f9b1841c2023c5b89309eb762eea70
TSgt Jason Leaver <jason.leaver@us.af.mil>
2022-10-18 17:40:37 +0000

    .

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 export PATH="${HOME}/venv/bin:${PATH}"
 sudo apt-get update -y
 sudo apt-get install zsh -y
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+# sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
